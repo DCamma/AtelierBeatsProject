46bda9e92f3546bae9719eaf834abc57da54447b
dossan <nelson.brochado@usi.ch>
2015-11-22 14:06:47 +0100

    Started creating the UI for exercise 9
    Added the new menu item for the activities, created the model for the
    activities, etc

---
+++ AtelierBeats/models/Activity.js
@@ -0,0 +1,40 @@
+/** 
+ * The Activity Model
+ * Schema:
+ * _id           String       required   unique identifier of the activity.
+ * timestamp      
+ * action  
+ *			here are the following actions to track and their corresponding target object:
+ *				- track playback (play, track URL)
+ *				- create new playlist (create_playlist, playlistURL)
+ * 				- delete track (delete_track, no trarget object)
+ *  
+ * target 
+ */
+
+'use strict';
+
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var ActivitySchema = new mongoose.Schema({
+        action: {
+            type: String, // for now lets use just a string...
+            required: true
+        },
+        target: {
+            type: String, // name of the target object on the action
+            required: true
+        },
+        timestamp: {
+            type: Date,
+            default: Date.now
+        },
+    }
+
+);
+
+//register model
+mongoose.model('Activity', ActivitySchema);
+
+module.exports = ActivitySchema;
