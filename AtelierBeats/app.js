5ce2bc5f3a1e15ce54ecdc572748708262a52f5f
dossan <nelson.brochado@usi.ch>
2015-11-27 16:07:10 +0100

    Changed app.js to require models before the calls to init.js, etc

--- AtelierBeats/app.js
+++ AtelierBeats/app.js
@@ -5,16 +5,28 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var dustjs = require('adaro');
 var app = express();
-
-// Configuring Passport
+var mongoose = require('mongoose');
 var passport = require('passport');
 var expressSession = require('express-session');
-app.use(expressSession({secret: 'mySecretKey'}));
+
+// Connect to MongoDB here
+mongoose.connect(config.mongoUrl + config.mongoDbName);
+
+// Register model definition here
+require('./models');
+
+// Configuring Passport
+app.use(expressSession({
+  secret: 'mySecretKey'
+}));
+
 app.use(passport.initialize());
+
+// for persistent login sessions
 app.use(passport.session());
 
- // Using the flash middleware provided by connect-flash to store messages in session
- // and displaying in templates
+// Using the flash middleware provided by connect-flash to store messages in session
+// and displaying in templates
 var flash = require('connect-flash');
 app.use(flash());
 
@@ -25,13 +37,6 @@ initPassport(passport);
 var routes = require('./routes/root/router.js')(passport);
 app.use('/', routes);
 
-// Connect to MongoDB here
-var mongoose   = require('mongoose');
-mongoose.connect(config.mongoUrl + config.mongoDbName);
-
-// Register model definition here
-require('./models');
-
 // dustjs view engine setup
 app.engine('dust', dustjs.dust());
 app.set('views', path.join(__dirname, 'views'));
@@ -39,13 +44,17 @@ app.set('view engine', 'dust');
 
 //configure app
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));    // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());    // parse application/json
+
+app.use(bodyParser.urlencoded({
+  extended: false
+})); // parse application/x-www-form-urlencoded
+
+app.use(bodyParser.json()); // parse application/json
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Initialize routers here
-
 var routers = require('./routes/routers');
+
 app.use('/', routers.root);
 app.use('/library', routers.library);
 app.use('/albums', routers.albums);
--- AtelierBeats/routes/root/router.js
+++ AtelierBeats/routes/root/router.js
@@ -1,18 +1,15 @@
 /** @module users/router */
 'use strict';
 
-var middleware =  require('../middleware');
+var middleware = require('../middleware');
 var rootUrl = require("../../config").url;
 
-
 //supported methods
-
 
 // router.get('/', function(req, res, next) {
 
 //   res.render('login');
 
-  
 // });
 
 // /** router for /users */
@@ -23,7 +20,7 @@ var router = express.Router();
 
 router.all('/', middleware.supportedMethods('GET, POST, PUT, OPTIONS'));
 
-var isAuthenticated = function (req, res, next) {
+var isAuthenticated = function(req, res, next) {
   // if user is authenticated in the session, call the next() to call the next request handler 
   // Passport adds this method to request object. A middleware is allowed to add properties to
   // request and response objects
@@ -33,36 +30,42 @@ var isAuthenticated = function (req, res, next) {
   res.redirect('/');
 }
 
-module.exports = function(passport){
+module.exports = function(passport) {
 
   /* GET login page. */
   router.get('/', function(req, res) {
-      // Display the Login page with any flash message, if any
-    res.render('login', { message: req.flash('message') });
+    // Display the Login page with any flash message, if any
+    res.render('login', {
+      message: req.flash('message')
+    });
   });
 
   /* Handle Login POST */
   router.post('/login', passport.authenticate('login', {
     successRedirect: '/library',
     failureRedirect: '/',
-    failureFlash : true  
+    failureFlash: true
   }));
 
   /* GET Registration Page */
-  router.get('/signup', function(req, res){
-    res.render('signup',{message: req.flash('message')});
+  router.get('/signup', function(req, res) {
+    res.render('signup', {
+      message: req.flash('message')
+    });
   });
 
   /* Handle Registration POST */
   router.post('/signup', passport.authenticate('signup', {
     successRedirect: '/library',
     failureRedirect: '/signup',
-    failureFlash : true  
+    failureFlash: true
   }));
 
   /* GET Home Page */
-  router.get('/library', isAuthenticated, function(req, res){
-    res.render('library', { user: req.user });
+  router.get('/library', isAuthenticated, function(req, res) {
+    res.render('library', {
+      user: req.user
+    });
   });
 
   /* Handle Logout */
@@ -73,6 +76,3 @@ module.exports = function(passport){
 
   return router;
 }
-
-
-
