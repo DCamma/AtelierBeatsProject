d81f4e016a0e01457e68ee2cb34cb1e81ef1b9fd
dossan <nelson.brochado@usi.ch>
2015-12-03 14:47:49 +0100

    Error messages no more different when inserting credentials at the login page

--- AtelierBeats/passport/login.js
+++ AtelierBeats/passport/login.js
@@ -28,14 +28,14 @@ module.exports = function(passport) {
             // Username does not exist, log the error and redirect back
             if (!user) {
               console.log('User Not Found with username ' + username);
-              return done(null, false, req.flash('message', 'User not found.'));
+              return done(null, false, req.flash('message', 'Incorrect username or password'));
             }
 
             // User exists but wrong password, log the error 
             if (!isValidPassword(user, password)) {
               console.log('Invalid Password');
               // redirect back to login page
-              return done(null, false, req.flash('message', 'Invalid Password'));
+              return done(null, false, req.flash('message', 'Incorrect username or password'));
             }
 
             // User and password both match, return user from done method
