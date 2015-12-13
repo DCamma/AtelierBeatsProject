fa10cad3fc2903621a884bac149602fa46804533
Epoes <leonardo.iandiorio@gmail.com>
2015-12-13 18:43:05 +0100

    little modifiche pt.2

--- AtelierBeats/public/js/utils.js
+++ AtelierBeats/public/js/utils.js
@@ -2,7 +2,6 @@ function formatTime(totalSec) {
   var hours = parseInt(totalSec / 3600) % 24;
   var minutes = parseInt(totalSec / 60) % 60;
   var seconds = (totalSec % 60);
-  console.log(totalSec, minutes, seconds)
   var result = (hours < 1 ? '' : hours + ':');
   result += (minutes < 1 ? "00:" : minutes + ':');
   result += (seconds < 10 ? "0" + seconds : seconds);
